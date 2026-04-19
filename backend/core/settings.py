950b2eec260e7580990d4e1f08242af5193ee4c4
avp1510 <avp1510@gmail.com>
2026-04-19 13:37:21 -0400

    Configure production SECRET_KEY from environment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,13 +16,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jyi6neu=+$c)_5=s3%2h3gx$m9x)q5yx@!gz^d^!#dan)(%z%w'
-
-import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jyi6neu=+$c)_5=s3%2h3gx$m9x)q5yx@!gz^d^!#dan)(%z%w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
